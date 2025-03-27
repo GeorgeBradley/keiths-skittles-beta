3062ab43d75b66dd0e3a9a42f4ee327729596cf3
George Bradley <georgeaustinbradley@outlook.com>
2025-03-27 20:59:43 +0000

    Assync stuff

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,6 @@ from django.contrib.auth import views as auth_views
 from .views import register
 
 urlpatterns = [
-    path("register/", register, name="register"),
     path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="past_games"), name="logout"),
 ]
